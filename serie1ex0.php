d7be1262d584de4030cf52ba3044eca8ac322f09
wolfx01 <itsbilalchouichou@gmail.com>
2026-03-30 23:09:39 +0100

    create  class point2D

--- serie1ex0.php
+++ serie1ex0.php
@@ -22,6 +22,30 @@ echo "this  : " . $p->toString();
 
 "<br>";
 echo"this  norm : ".$p->norme()
+}
+class  Point2D{
+    private int  $x  , $y ;
+    public function __construct($x , $y){
+        $this->x = $x;
+        $this->y = $y; 
+
+    } 
+    public function toString(): string{
+        return "POINT( $this->x  , $this->y)"; 
+
+    }
+    public function norme():float{
+        return sqrt($this->x ** 2 + $this->y ** 2);
+    }
+}
+
+
+$p = new Point(2 , 4) ;
+echo "this  : " . $p->toString();
+
+
+"<br>";
+echo"this  norm : ".$p->norme()
 
 
 
